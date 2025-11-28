563b00b7798ac319493e1da84f25f395c41234cb
Spexophis <billinpro@gmail.com>
2025-11-28 17:45:56 +0100

    Update run_threads.py

--- run_threads.py
+++ run_threads.py
@@ -1,35 +1,5 @@
 import numpy as np
 from PyQt6.QtCore import QThread, pyqtSignal
-from PyQt6.QtCore import QObject, QMutex, QMutexLocker, pyqtSlot
-
-class FramePool(QObject):
-    def __init__(self, shape=(1024, 1024), dtype=np.uint16, n_buffers=4):
-        super().__init__()
-        self._buffers = [np.empty(shape, dtype=dtype) for _ in range(n_buffers)]
-        self._free = list(range(n_buffers))
-        self._in_use = set()
-        self._m = QMutex()
-
-    def acquire(self):
-        """Reserve a buffer index for writing. Returns idx or None if none free."""
-        with QMutexLocker(self._m):
-            if not self._free:
-                return None
-            idx = self._free.pop()
-            self._in_use.add(idx)
-            return idx
-
-    def buffer(self, idx: int) -> np.ndarray:
-        return self._buffers[idx]
-
-    @pyqtSlot(object)
-    def release(self, token):
-        """Return a buffer to the free list after viewer consumed/discarded it."""
-        idx = int(token)
-        with QMutexLocker(self._m):
-            if idx in self._in_use:
-                self._in_use.remove(idx)
-                self._free.append(idx)
 
 
 class AcquisitionThread(QThread):
